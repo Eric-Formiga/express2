6926a40db9b2e7cb5244d1aff4597057fa639124
Eric-Formiga <eric.13formiga@gmail.com>
2024-12-06 20:52:21 -0300

    feat: add POST url

--- params/index.js
+++ params/index.js
@@ -5,6 +5,26 @@ const port = 3000
 const path = require('path')
 const basePath = path.join(__dirname, 'templates')
 
+app.use(express.urlencoded({
+  extended:true,
+}))
+
+app.use(express.json())
+
+app.get('/users/add', (req, res) => {
+
+  res.sendFile(`${basePath}/userform.html`)
+
+})
+
+app.post('/users/save', (req, res) => {
+  const name = req.body.name
+  const age = req.body.age
+  console.log(`${name} ${age}`)
+
+  res.sendFile(`${basePath}/userform.html`)
+})
+
 app.get('/users/:id', (req, res) => {
   const id = req.params.id
   console.log(id)
@@ -12,13 +32,10 @@ app.get('/users/:id', (req, res) => {
 
 })
 
-
 app.get('/', (req, res) => {
   res.send("Olá mundo!")
 
 })
-
-
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
