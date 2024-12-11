dea41287457956a6db9a3e22537964fd2e1a49ba
Eric-Formiga <eric.13formiga@gmail.com>
2024-12-11 19:24:45 -0300

    feat: add handlebars

--- Template_engine/hanblebars/index.js
+++ Template_engine/hanblebars/index.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Configurando o Handlebars como template engine
+app.engine('handlebars', exphbs.engine()); // Registro do mecanismo de template
+app.set('view engine', 'handlebars'); // Definindo o view engine como Handlebars
+
+// Rota principal
+app.get('/', (req, res) => {
+  res.render('home', { layout: false }); // Renderiza o template `home.handlebars`
+});
+
+// Iniciando o servidor
+app.listen(3000, () => {
+  console.log('App funcionando na porta 3000!');
+});
